ceaf73a79ba158ab3def2cd3a176d889a5a945e2
fhomaia <fhomaia@gmail.com>
2024-09-17 18:44:17 -0300

    add assets to public folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/React-Review/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
+  plugins: [react()]
 })
 
