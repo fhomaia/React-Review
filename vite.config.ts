d377d6634f20556f49f8c2f989fe9cf7d5063956
fhomaia <fhomaia@gmail.com>
2024-09-17 19:03:04 -0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/React-Review/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets', // Certifique-se de que isso está configurado corretamente
+  }
 })
-
